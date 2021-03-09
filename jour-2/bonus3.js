3d32d81b2cf152ab179f503e08b41d931e1d00ae
unknown <suleman.khn10@gmail.com>
2021-03-09 17:31:15 +0100

    javascript jour 2

--- jour-2/bonus3.js
+++ jour-2/bonus3.js
@@ -2,3 +2,11 @@ var roundedNumber = 3.6;
 var number = Math.floor(roundedNumber); 
 var results = roundedNumber - number;
 console.log(results);
+
+if (results <= 0.5)
+{
+    console.log(Math.floor(roundedNumber));
+}
+else {
+    console.log(Math.ceil(roundedNumber));
+}
