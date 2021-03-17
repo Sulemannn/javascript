f6ca3e212d4a33468a02fd6585cd8034d765351d
unknown <suleman.khn10@gmail.com>
2021-03-17 16:44:41 +0100

    exo table binome

--- jour-5/table/table.js
+++ jour-5/table/table.js
@@ -1,16 +1,17 @@
 function multiply(nmb){
     if(typeof(num) == "number"  && process.argv.length == 3 ){
         var a =parseInt(num);
-        var result =0
+        var result = 0
         for(var i = 0; i <= 10; i++){
             result = i*a
             console.log(num,"*",i,"=", result)
         }
     }
-    else{
+    else {
         console.log("error")
     }
-}
+};
 
 var num = parseInt(process.argv[2])
-multiply(num)
+
+multiply(num);
