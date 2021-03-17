8d88f616f1fd0e81775d39a3e101930331fe61de
unknown <suleman.khn10@gmail.com>
2021-03-17 17:33:47 +0100

    exo binome JOUR

--- jour-5/table/table.js
+++ jour-5/table/table.js
@@ -1,17 +1,13 @@
-function multiply(nmb){
-    if(typeof(num) == "number"  && process.argv.length == 3 ){
-        var a =parseInt(num);
-        var result = 0
-        for(var i = 0; i <= 10; i++){
-            result = i*a
-            console.log(num,"*",i,"=", result)
-        }
-    }
-    else {
-        console.log("error")
-    }
-};
+var {multiply} = require("../exo3/table-utils")
+var num =parseInt(process.argv[2])
 
-var num = parseInt(process.argv[2])
 
-multiply(num);
+if (typeof (num) == "number" && process.argv.length == 3) {
+    
+    multiply(num)
+
+}
+
+else{
+    console.log("error")
+}
