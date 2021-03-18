19a7e6049b3baaa742eab7d70be407f9f4961123
unknown <suleman.khn10@gmail.com>
2021-03-18 17:06:14 +0100

    exo binome jour-6

--- jour-6/age.js
+++ jour-6/age.js
@@ -4,3 +4,13 @@
 // - La fonction doit calculer l'âge de la personne (en années) née a ce moment là, en fonction de la date d'aujourd'hui
 // - Appelez la fonction et affichez son résultat dans la console
 
+function calculateAge(str) {
+    var demo = new Date().getFullYear();
+    //console.log(demo);
+    var année = parseInt(str.slice(6, 10))
+    //console.log(anne)
+    var age = demo - année
+    console.log("Tu as",age,"ans !")
+
+}
+calculateAge("01/08/2000")
