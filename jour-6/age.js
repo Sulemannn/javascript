9ef74665774b562dc8d4d66be5ac8d473cc35919
unknown <suleman.khn10@gmail.com>
2021-03-18 16:29:40 +0100

    exo jour-6 binome

---
+++ jour-6/age.js
@@ -0,0 +1,6 @@
+// 02 - Age
+
+// - Créez une fonction `calculateAge` qui reçoit en paramètre une date au format string (qui sert de date de naissance)
+// - La fonction doit calculer l'âge de la personne (en années) née a ce moment là, en fonction de la date d'aujourd'hui
+// - Appelez la fonction et affichez son résultat dans la console
+
---
+++ jour-6/format.js
@@ -0,0 +1,26 @@
+// 01 - Format
+
+// - Créez une fonction `formatDate` qui reçoit en paramètre une string (au format aaaa-mm-jj)
+// - La fonction doit renvoyer la date au format jj/mm/aaaa
+// - Appelez la fonction et affichez son résultat dans la console
+
+function formatDate(str) {
+
+    var x = str.split("-").reverse().join("/")
+    console.log(x)
+
+}
+
+formatDate("1999-04-03")
+
+
+
+
+
+
+
+
+
+
+
+
