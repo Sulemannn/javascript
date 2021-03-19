cd897c841385b5d3cfccf94344c5d1a0ae52eed2
unknown <suleman.khn10@gmail.com>
2021-03-19 14:12:15 +0100

    time.js jour-4

--- jour-4/time.js
+++ jour-4/time.js
@@ -1,18 +1,51 @@
+function format(num) {
+    // 124 secondes => 2m et 4s
+    // je sais que 1m = 60s
+    /*
+    console.log(Math.floor(num / 3600)); // heures
+    console.log(num % 3600) // minutes et les secondes
+    console.log(Math.floor(num % 3600 / 60));
+    console.log(num % 3600 % 60);
+    */
 
-function format(time) {
-    var hrs = ~~(time / 3600);
-    var mins = ~~((time % 3600) / 60); 
-    var secs = ~~time % 60;
-
-    var ret = "";
-    if (hrs > 0) {
-        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-    }
-    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-    ret += "" + secs;
-    return ret;
+    var hours = Math.floor(num / 3600);
+    var restHours = num % 3600;
+    var minutes = Math.floor(restHours / 60);
+    var secondes = restHours % 60;
+    console.log(`${hours} : ${minutes} : ${secondes}`);
 }
 
-console.log(format(3700));
+function formatWithoutMaths(num) {
+    var days = 0;
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
+    for (var i = 0; i < num; i++) {
+        seconds++;
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+        if (minutes === 60) {
+            hours++;
+            minutes = 0;
+        }
+        if (hours === 24) {
+            days++;
+            hours = 0;
+        }
+    }
+    console.log(`${hours} : ${minutes} : ${seconds}`);
+}
 
+function mehdiFormat(num) {
+    var hours = Math.floor(num / 3600);
+    var rest = num - hours * 3600;
+    var minutes = Math.floor(rest / 60);
+    var seconds = rest - minutes * 60;
+    console.log(`${hours} : ${minutes} : ${seconds}`);
+}
 
+format(3700);
+formatWithoutMaths(3700);
+mehdiFormat(3700);
