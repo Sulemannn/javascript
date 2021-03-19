302f1326e5594fa82d9130cfa1e5b80987daae78
unknown <suleman.khn10@gmail.com>
2021-03-19 14:06:46 +0100

    combine.js jour-4

--- jour-4/combine.js
+++ jour-4/combine.js
@@ -2,21 +2,31 @@ var cat = {
     name: "Garfield",
     age: 3,
     isCute: true, 
-}
+};
 
 var cat2 = {
-    name: "Miaouss",
+    name: "Félix",
     age: 5,
     isCute: false,
-}
+    // properties: {
+    //     color: "black & white",
+    //     jump: 15
+    // }
+};
 
-var cats = [ cat, cat2 ]
+var cats = [cat, cat2];
 
-// Affichez la valeur de age de cat dans la console
+// console.log(cats)
 
-console.log(cat.age);
+console.log("Age of cat", cat.age)
+console.log("Age of cat 2", cats[0].age)
 
-// Affichez la valeur de isCute de cat2 dans la console
 
-console.log(cat2.isCute);
+console.log("cat2 isCute? ", cat2.isCute);
+console.log("cat2 isCute? 2", cat2["isCute"])
+console.log("cat2 isCute? 3", cats[1].isCute)
+console.log("cat2 isCute? 4", cats[1]["isCute"])
 
+var keyName = "isCute"
+console.log("cat2 isCute? 5", cat2[keyName])
+console.log("cat2 isCute? 6", cats[1][keyName])
