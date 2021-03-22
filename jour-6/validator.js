ecce9bd61c9d12a028627315621d583b451216ab
unknown <suleman.khn10@gmail.com>
2021-03-22 15:36:15 +0100

    validator.js jour-6

--- jour-6/validator.js
+++ jour-6/validator.js
@@ -1,13 +1,33 @@
+
 var prompt = require("prompt");
 
-fucntion checkProfile() {
-    
-    prompt.get(
-
-        {
-            email: "myEmail"
-            username: "myUserName"
-            password: "myPassword"
+var schema = {
+  properties: {
+    email: { pattern: /^\S+@\S+\.\S+$/, required: true, message:"Email non valide" },
+    username: {pattern: /^[a-zA-Z-]+$/, required: true, message:"Username non valide"},
+    password: {required: true, message:"Password non valide", conform: function (value) {
+        if (value.search(/.{6,}/) === -1) {
+            return false;
         }
-    )
-}
+
+        if (value.search(/[a-zA-Z]/) === -1) {
+            return false;
+        }
+
+        if (value.search(/[0-9]/) === -1) {
+            return false;
+        }
+
+        if (value.search(/[^a-zA-Z0-9-]/) > -1) {
+            return false;
+        }
+
+        return true;
+    }}
+  },
+};
+
+prompt.start();
+prompt.get(schema, (err, res) => {
+    console.log(res);
+});
